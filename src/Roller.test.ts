e4b5395cac2f944209bb6bb718e6192a3117f92f
hardikpatil <hardik.patil2494@gmail.com>
2023-03-28 10:36:20 -0700

    added test cases and coverage

--- src/Roller.test.ts
+++ src/Roller.test.ts
@@ -1,12 +1,60 @@
 import {Roller} from './Roller';
 
-describe("Smoke test", ()=> {
-  test("The test scaffold runs successfully.", ()=> {
-    expect(true).toBe(true);
-  });
-})
+describe('Roller', () => {
+  let roller: Roller;
 
-describe("Roller tests", ()=> {
-  test("Description", () => {
+  beforeEach(() => {
+    roller = new Roller(6);
+  });
+
+  test('Roller should have a specified number of faces', () => {
+    expect(roller['_faces']).toBe(6);
+  });
+
+  test('Roller should default to 6 faces if less than 2 faces are specified', () => {
+    const roller2 = new Roller(1);
+    expect(roller2['_faces']).toBe(6);
+  });
+
+  test('Roller should record a valid roll', () => {
+    const value = roller.roll(3);
+    expect(value).toBe(3);
+    expect(roller.last()).toBe(3);
+    expect(roller.distribution().get(3)).toBe(1);
+  });
+
+  test('Roller should not record an invalid roll', () => {
+    const value = roller.roll(10);
+    expect(value).toBe(0);
+    expect(roller.last()).toBe(0);
+    expect(roller.distribution().get(1)).toBe(0);
+    expect(roller.distribution().get(2)).toBe(0);
+    expect(roller.distribution().get(3)).toBe(0);
+    expect(roller.distribution().get(4)).toBe(0);
+    expect(roller.distribution().get(5)).toBe(0);
+    expect(roller.distribution().get(6)).toBe(0);
+    expect(roller.distribution().get(10)).toBeUndefined();
+
+  //   const value = roller.roll(10);
+  // expect(value).toBe(0);
+  // expect(roller.last()).toBe(0);
+  // const distribution = roller.distribution();
+  // expect(distribution instanceof Map).toBe(true);
+  // expect(distribution.get(1)).toBe(0);
+  // expect(distribution.get(2)).toBe(0);
+  // expect(distribution.get(3)).toBe(0);
+  // expect(distribution.get(4)).toBe(0);
+  // expect(distribution.get(5)).toBe(0);
+  // expect(distribution.get(6)).toBe(0);
+  // expect(distribution.get(10)).toBeUndefined();
+
+  });
+
+  test('Roller should update the distribution of rolls', () => {
+    roller.roll(3);
+    roller.roll(3);
+    roller.roll(4);
+    expect(roller.distribution().get(3)).toBe(2);
+    expect(roller.distribution().get(4)).toBe(1);
   });
 });
